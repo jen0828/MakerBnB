5b14cb6211903c626d4dde4834d5d3162bca283f
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-06-29 13:12:11 +0100

    Added user database spec and main code

--- lib/user.rb
+++ lib/user.rb
@@ -6,4 +6,10 @@ class User
 		@email = email
 		@password = password
 	end
+	
+	def self.create(name:, email:, password:)
+		connection = PG.connect(dbname: makersbnb_test)
+		result = connection.exec("INSERT INTO user (name, email, password) VALUES ('#{name}', '#{email}', '#{password}') RETURNING name, email, password;")
+		User.new(name: result[0]['name'], email: result[0]['email'], password: result[0]['password'])
+	end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,4 +1,5 @@
 require 'user'
+require 'pg'
 
 RSpec.describe User do
 	let(:user) { User.new("Fabio", "fabio@gmail.com", "Fabio123!")}
@@ -9,4 +10,12 @@ RSpec.describe User do
 		expect(user.password).to eq('Fabio123!')
 	end
 
+	it 'creates a new database' do
+		connection = PG.connect(dbname: 'makersbnb_test')
+		guest = User.create(name: 'Fabio', email: 'fabio@gmail.com', password: 'Fabio123!')
+		
+		expect(guest.name).to eq('Fabio')
+		expect(guest.email).to eq('fabio@gmail.com')
+		expect(guest.password).to eq('Fabio123!')
+	end
 end
