c9e3e062f4efa2223cdb23b68d3c37b3819cdf2c
Frank Mckenna <fmckenna56330@gmail.com>
2021-06-29 15:36:33 +0100

    completed user story 2

--- lib/space.rb
+++ lib/space.rb
@@ -1,6 +1,5 @@
 require 'pg'
 
-
 class Space
   attr_reader :name, :description, :price, :start_date, :finish_date
 
@@ -13,10 +12,17 @@ class Space
   end
 
   def self.create(name:, description:, price:, start_date:, finish_date:)
-    connection = PG.connect(dbname: space_test)
+    connection = PG.connect(dbname: 'makersbnb')
     result = connection.exec("INSERT INTO space (name, description, price, start_date, finish_date) VALUES ('#{name}', '#{description}', #{price}, '#{start_date}', '#{finish_date}') RETURNING name, description, price, start_date, finish_date;")
-    Space.new(name: result[0]['name'], description: result[0]['description'], price: result[0]['price'], stat_date: result[0]['start_date'], finish_date: result[0]['finish_date'])
+    Space.new(name: result[0]['name'], description: result[0]['description'], price: result[0]['price'], start_date: result[0]['start_date'], finish_date: result[0]['finish_date'])
   end
 
+  def self.listing
+    connection = PG.connect(dbname: 'makersbnb')
+    result = connection.exec("SELECT * FROM space;")
+     result.map do |space| 
+       Space.new(name: space['name'], description: space['description'], price: space['price'], start_date: space['start_date'], finish_date: space['finish_date'])
+     end
+  end
 
 end
