4d3f9fa41add3f8b53e4ae07395172665a487d7d
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-06-30 15:24:28 +0100

    Added checking date range feature test

--- spec/features/checking_date_range_spec.rb
+++ spec/features/checking_date_range_spec.rb
@@ -1,6 +1,12 @@
 feature 'Date range' do
   scenario 'User can see available bookings spaces' do
-    visit('/spaces/new')
+    visit("/")
+    fill_in("email", with: "fabio@gmail.com")
+    fill_in("name", with: "Fabio")
+    fill_in("password", with: "123")
+    click_button("Sign up")
+
+    click_button('List a space')
     fill_in('name', with: 'Beach house')
     fill_in('description', with: 'cozy with sea view')
     fill_in('price', with: '5')
@@ -8,8 +14,8 @@ feature 'Date range' do
     fill_in('finish_date', with: '2021-09-28')
     click_button('List my spaces')
 
-    select '2021-08-28', :from => 'start_date'
-    select '2021-09-28', :from => 'finish_date'
+    fill_in('start_date', with: '2021-08-28')
+    fill_in('finish_date', with: '2021-09-28')
     click_button('Check availability')
 
     expect(page).to have_content('Beach house')
