fb16c81456aa182a83791f45a956b512bb44d5e0
Fabio <alessf13@gmail.com>
2021-06-30 15:55:08 +0200

    DbConnect class created to refactor DB connection for both testing and live environment

--- lib/space.rb
+++ lib/space.rb
@@ -1,6 +1,6 @@
-require 'pg'
+require_relative 'db_connection'
 
-class Space
+class Space < DbConnect
   attr_reader :name, :description, :price, :start_date, :finish_date
 
   def initialize(name:, description:, price:, start_date:, finish_date:)
@@ -12,27 +12,19 @@ class Space
   end
 
   def self.create(name:, description:, price:, start_date:, finish_date:)
-    if ENV['ENVIRONMENT'] == 'test'
-			connection = PG.connect(dbname: 'makersbnb_test')
-		else
-			connection = PG.connect(dbname: 'makersbnb')	
-		end
+    connection = DbConnect.new.connect
 
     result = connection.exec("INSERT INTO space (name, description, price, start_date, finish_date) VALUES ('#{name}', '#{description}', #{price}, '#{start_date}', '#{finish_date}') RETURNING name, description, price, start_date, finish_date;")
     Space.new(name: result[0]['name'], description: result[0]['description'], price: result[0]['price'], start_date: result[0]['start_date'], finish_date: result[0]['finish_date'])
   end
 
   def self.listing
-    if ENV['ENVIRONMENT'] == 'test'
-			connection = PG.connect(dbname: 'makersbnb_test')
-		else
-			connection = PG.connect(dbname: 'makersbnb')	
-		end
+    connection = DbConnect.new.connect
 
     result = connection.exec("SELECT * FROM space LIMIT 6;")
-     result.map do |space| 
-       Space.new(name: space['name'], description: space['description'], price: space['price'], start_date: space['start_date'], finish_date: space['finish_date'])
-     end
+      result.map do |space| 
+        Space.new(name: space['name'], description: space['description'], price: space['price'], start_date: space['start_date'], finish_date: space['finish_date'])
+      end
   end
 
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,6 +1,6 @@
-require 'pg'
+require_relative 'db_connection'
 
-class User
+class User < DbConnect
 	attr_reader :id, :name, :email, :password
 
 	def initialize(id:, name:, email:, password:)
@@ -11,22 +11,14 @@ class User
 	end
 	
 	def self.create(name:, email:, password:)
-		if ENV['ENVIRONMENT'] == 'test'
-			connection = PG.connect(dbname: 'makersbnb_test')
-		else
-			connection = PG.connect(dbname: 'makersbnb')	
-		end
+		connection = DbConnect.new.connect
 
 		result = connection.exec("INSERT INTO guest (name, email, password) VALUES ('#{name}', '#{email}', '#{password}') RETURNING id, name, email, password;")
 		User.new(id: result[0]['id'], name: result[0]['name'], email: result[0]['email'], password: result[0]['password'])
 	end
 
 	def self.find(name)
-		if ENV['ENVIRONMENT'] == 'test'
-			connection = PG.connect(dbname: 'makersbnb_test')
-		else
-			connection = PG.connect(dbname: 'makersbnb')	
-		end
+		connection = DbConnect.new.connect
 		
 		result = connection.query("SELECT * FROM guest WHERE name = '#{name}';")
 		@name = result[0]['name']
