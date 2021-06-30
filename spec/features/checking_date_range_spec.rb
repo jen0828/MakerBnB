dc29a114f624fde38c446848580efae3448824da
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-06-30 11:39:00 +0100

    Added date range feature test

--- spec/features/checking_date_range_spec.rb
+++ spec/features/checking_date_range_spec.rb
@@ -1,13 +1,16 @@
-feature 'Adding a new space' do
-  scenario 'A user can add a space to Makers BnB' do
+feature 'Date range' do
+  scenario 'User can see available bookings spaces' do
     visit('/spaces/new')
     fill_in('name', with: 'Beach house')
     fill_in('description', with: 'cozy with sea view')
     fill_in('price', with: '$5')
     fill_in('start_date', with: '2021-08-28')
-    fill_in('finish_date', with: '2021-09-28')
+    fill_in('start_date', with: '2021-09-28')
     click_button('List my spaces')
-    click_button('view spaces')
+
+    visit ('/listing')
+    select '2021-08-28', :from => 'start_date'
+    select '2021-09-28', :from => 'finish_date'
 
     expect(page).to have_content('Beach house')
   end
