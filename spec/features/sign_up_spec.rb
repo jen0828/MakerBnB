2c2df139d04985c6e4e6e4d85cb67b76f622d7a0
Frank Mckenna <fmckenna56330@gmail.com>
2021-07-01 08:59:01 +0100

    changed main page layout and added styles

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -6,6 +6,6 @@ feature 'sigining up' do
     fill_in("password", with: "123")
     click_button("Sign up") 
 
-    expect(page).to have_content("Welcome, Fabio!")
+    expect(page).to have_content("Logged in as, Fabio!")
   end
 end
